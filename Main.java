0ff29b76c74ab04f47a9fa9025e735349cb05d8c
dlyons5 <dominic.lyons@patelhs.org>
2020-09-29 14:30:16 +0000

    The whole project.

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
-class Main {
+class CheckMyAge {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    int myAge = 15;
+    int myBirthday = 517;  
+    int todaysDate = 921;
+  
+    if (myBirthday == todaysDate)
+    {
+      myAge++;
+      System.out.println(" I am " + myAge + " years old ");
+    }
+    else
+    { 
+      System.out.println(" I am still " + myAge + "years old");
+    }  
   }
 }
